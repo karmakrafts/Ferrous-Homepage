16a73df2b0393c17575b9f1a5b3758ecd1adf426
KitsuneAlex <alexh@karmakrafts.dev>
2023-11-24 05:10:16 +0100

    Implement smooth background transition on lading page using some simple JavaScript

--- js/index.js
+++ js/index.js
@@ -3,6 +3,30 @@
  * @since 22/11/2023
  */
 
-function init() {
+let wasScrolledBefore = false
 
+function init() {
+    const scrollContainer = document.getElementById('scrollContainer')
+    const backgroundContainer = document.getElementById('backgroundContainer')
+    let prevScrollTop = 0
+    scrollContainer.onscroll = () => {
+        const scrollTop = scrollContainer.scrollTop
+        if(scrollTop > prevScrollTop && scrollTop >= ((window.innerHeight / 3) << 1)) {
+            if(wasScrolledBefore) {
+                prevScrollTop = scrollTop
+                return
+            }
+            backgroundContainer.classList.add('visible')
+            wasScrolledBefore = true
+        }
+        else if(scrollTop < prevScrollTop && scrollTop <= window.innerHeight) {
+            if(!wasScrolledBefore) {
+                prevScrollTop = scrollTop
+                return
+            }
+            backgroundContainer.classList.remove('visible')
+            wasScrolledBefore = false
+        }
+        prevScrollTop = scrollTop
+    }
 }
